42ccbf173ab78c312fb50f9c3b3d6de140e0b5e0
Matheus Patricio <patriciomtt@outlook.com>
2024-11-18 22:38:17 -0300

    modificadores ref e out

---
+++ modificadores/modificadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using modificadores;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            int x = 10;
+            Console.WriteLine("-----------------REF-----------------");
+
+
+            calculadora.Triple(ref x);
+            Console.WriteLine(x);
+
+            Console.WriteLine("-----------------OUT-----------------");
+
+            int y = 10;
+            int quad;
+            calculadora.Double(y, out quad);
+            Console.WriteLine(quad);
+        }
+    }
+}
---
+++ modificadores/modificadores/calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modificadores
+{
+    class calculadora
+    {
+
+        public static void Triple(ref int x)
+        {
+            x = x * x;
+        }
+        public static void Double(int origin, out int result)
+        {
+            result = origin * 10;
+        }
+
+    }
+}
